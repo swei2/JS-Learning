f9378668ff343b27d6e3a1211623092bf353e986
swei2 <swei2@masonlive.gmu.edu>
2023-07-28 18:56:11 -0400

    add a line

--- 99 Bottles.js
+++ 99 Bottles.js
@@ -1,6 +1,7 @@
 var TotalBottle = 99;
 while (TotalBottle >=0){
     var deductBottle = TotalBottle-1
+    console.log("this is my code");
     if (deductBottle >=0){
     console.log(TotalBottle + " bottles of beer on the wall," + TotalBottle + " bottles of beer. Take 1 down, pass it around,"+ deductBottle +" bottles of beer on the wall.");
     }
